f148bc591e2618c7beb71334f01a78adbb454a98
James <jamesedwardmcglade@gmail.com>
2020-07-01 11:59:59 +0100

    user population

--- EmployeeDirectory/Methods/Methods.cs
+++ EmployeeDirectory/Methods/Methods.cs
@@ -12,41 +12,41 @@ namespace EmployeeDirectory
             {
 
                 //Emily & Cals databases
-                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-                builder.DataSource = "localhost,1433";
-                builder.UserID = "sa";
-                builder.Password = "<YourStrong@Passw0rd>";
-                builder.InitialCatalog = "StaffDirectory";
-
-                //James database
                 //SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
                 //builder.DataSource = "localhost,1433";
                 //builder.UserID = "sa";
-                //builder.Password = "Puerr0r@diactiv0";
+                //builder.Password = "<YourStrong@Passw0rd>";
                 //builder.InitialCatalog = "StaffDirectory";
+
+                //James database
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "localhost,1433";
+                builder.UserID = "sa";
+                builder.Password = "Puerr0r@diactiv0";
+                builder.InitialCatalog = "StaffDirectory";
 
                 //what would you like to send to SQL?
                 string SELECT = "SELECT * FROM StaffModel";
 
-        // writing to the console before the connection is attempted
-        Console.WriteLine("Connecting to SQL Server.");
+                // writing to the console before the connection is attempted
+                Console.WriteLine("Connecting to SQL Server.");
 
                 // creating a new instance of the SqlConnection to actually connect to our DB.
                 SqlConnection connection = new SqlConnection(builder.ConnectionString);
                 using (connection)
                 {
                     SqlCommand cmd = new SqlCommand(SELECT, connection);
-    connection.Open();
+                    connection.Open();
 
                     using (SqlDataReader reader = cmd.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            for (int i = 0; i<reader.FieldCount; i++)
+                            for (int i = 0; i < reader.FieldCount; i++)
                             {
-        Console.WriteLine(reader.GetValue(i));
+                                Console.WriteLine(reader.GetValue(i));
                             }
-}
+                        }
                     }
                     Console.WriteLine("Done.");
                 }
@@ -69,11 +69,11 @@ namespace EmployeeDirectory
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
                 builder.DataSource = "localhost,1433";
                 builder.UserID = "sa";
-                builder.Password = "<YourStrong@Passw0rd>";
+                builder.Password = "Puerr0r@diactiv0";
                 builder.InitialCatalog = "StaffDirectory";
 
                 //what would you like to send to SQL?
-                string INSERT = "INSERT INTO StaffModel VALUES (10, 'Stuart Silverman', 'HR Director', 'Soapworks', 'stuart.silverman@talktalkplc.com', '0765432119'), (11, 'Roy Hawkins', 'HelpDesk', 'London', 'roy.hawkins@talktalkplc.com', '0765432119')";
+                string INSERT = "INSERT INTO StaffModel VALUES (4, 'Saul Goodman', 'Legal Department', 'Soapworks', 'better.callsaul@talktalkplc.com', '0765400998'), (5, 'Brian Kempt', 'Digital Line Manager', 'Soapworks', 'brian.kempt@talktalkplc.com', '0765436478'), (6, 'Nelson Van Alden', 'Finances', 'Edinburgh', 'nelson.vanalden@talktalkplc.com', '0765400322'), (7, 'John Goode', 'Customer Service', 'Soapworks', 'johnnyb.goode@talktalkplc.com', '0765430572')";
 
 
 
@@ -106,7 +106,7 @@ namespace EmployeeDirectory
             {
                 // Emily & Cals databases
                 SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
-        builder.DataSource = "localhost,1433";
+                builder.DataSource = "localhost,1433";
                 builder.UserID = "sa";
                 builder.Password = "<YourStrong@Passw0rd>";
                 builder.InitialCatalog = "StaffDirectory";
@@ -114,15 +114,15 @@ namespace EmployeeDirectory
                 //what would you like to send to SQL?
                 string UPDATE = "UPDATE StaffModel SET PhoneNumber = '0754321545' WHERE EmployeeID = 6";
 
-        // writing to the console before the connection is attempted
-        Console.WriteLine("Connecting to SQL Server.");
+                // writing to the console before the connection is attempted
+                Console.WriteLine("Connecting to SQL Server.");
 
                 // creating a new instance of the SqlConnection to actually connect to our DB.
                 SqlConnection connection = new SqlConnection(builder.ConnectionString);
                 using (connection)
                 {
                     SqlCommand cmd = new SqlCommand(UPDATE, connection);
-    connection.Open();
+                    connection.Open();
                     cmd.ExecuteNonQuery();
 
                     Console.WriteLine("Done.");
